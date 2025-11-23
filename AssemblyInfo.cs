10fd670909f5aad49e80b6d8dc3cad0c1ea7cebd
Fred Emmott <git@fredemmott.com>
2025-11-23 10:44:24 -0600

    Set version to 1.3.2.1

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Windows;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: AssemblyFileVersion("1.3.2.0")]
-[assembly: AssemblyVersion("1.3.2.0")]
+[assembly: AssemblyFileVersion("1.3.2.1")]
+[assembly: AssemblyVersion("1.3.2.1")]
